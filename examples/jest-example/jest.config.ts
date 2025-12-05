1b81605920c145c20d1cd48d33ea72801ced5d6d
Gabriele <gabrielepartiti+github@icloud.com>
2025-12-05 09:40:00 +0100

    feat: add jest example project

---
+++ examples/jest-example/jest.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  testEnvironment: 'node',
+  extensionsToTreatAsEsm: ['.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+  },
+  transform: {
+    '^.+\\.ts$': [
+      'ts-jest',
+      {
+        useESM: true,
+        diagnostics: false,
+      },
+    ],
+  },
+  testTimeout: 15000,
+};
+
+export default config;
